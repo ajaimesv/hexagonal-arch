23c227376ec8e7f1d8965f3b2580b013b53b9589
Andres Jaimes <andres@jaimes.net>
2022-07-23 00:39:00 -0400

    fix typos

--- adapters/repository/src/main/scala/users/adapters/repository/UserDb.scala
+++ adapters/repository/src/main/scala/users/adapters/repository/UserDb.scala
@@ -23,10 +23,10 @@ class UserDb extends UserRepository {
     database(id.value).toUser
 
   /*
-   * We pass fields instead of case classes because we may only allow to update some
+   * We pass fields instead of case classes because we may only need to update some
    * fields. Additionally, some fields may exist only in the repository layer (like
    * createdAt and updatedAt) which cannot be updated in other layers.
-   * By passing specific fields we can create other functions like updatePassword,
+   * By passing specific fields we can create specific functions like updatePassword,
    * updateName, etc.
    */
   override def update(id: Id, name: Name, username: Username, password: Password): User = {
